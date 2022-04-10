e2247e722ce7573a4e409f736c5998afbb424e27
Franklin Yin <yin4thewin@gmail.com>
2022-04-10 05:32:19 -0400

    initial upload

---
+++ App.js
@@ -0,0 +1,73 @@
+import * as React from 'react';
+import MapView, {Marker} from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import * as Location from 'expo-location';
+const axios = require('axios');
+
+export default function App() {
+  const [initialRegion, setInitialRegion] = React.useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  })
+  const [markers, setMarkers] = React.useState([])
+
+    React.useEffect(() => {
+      (async () => {
+        axios.get('https://cockroachapp.herokuapp.com/')
+        .then(function (response) {
+          // handle success
+          console.log("success",response);
+          response.data.forEach(element=>{
+            setMarkers([...markers, {title:"router",coord:{latitude: element.latitude, longitude: element.longitude?element.longitude:element.longtitude}}])
+          })
+        })
+        .catch(function (error) {
+          // handle error
+          console.log("error",error);
+        })
+        .then(function () {
+          // always executed
+        });
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          setErrorMsg('Permission to access location was denied');
+          console.log("not granted")
+          return;
+        }
+        let location = await Location.getCurrentPositionAsync({});
+        console.log("granted")
+        setInitialRegion({latitude: location.coords.latitude, longitude: location.coords.longitude, latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,})
+       // setLocation(location);
+      })();
+    }, []);
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map} region={initialRegion} onRegionChangeComplete={(region)=>{setInitialRegion(region)}}>
+        {markers.map((marker, index) => (
+          <Marker
+            key={index}
+            coordinate={marker.coord}
+            title={marker.title}
+            description={marker.description}
+          />
+        ))}
+      </MapView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
